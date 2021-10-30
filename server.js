a625256727bc5fb234ebf7826fe6902d24a45df5
junhee916 <junhee916@naver.com>
2021-10-30 13:23:24 +0900

    success signup setting ejs

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const path = require('path')
 const app = express()
 
 const userRouter = require('./routes/user')
 
 const connectDB = require('./config/database')
 connectDB()
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use('/publics', express.static(__dirname + '/publics'))
 
 // middleware
 app.use(bodyParser.json())
@@ -16,6 +22,16 @@ app.use(bodyParser.urlencoded({extended : false}))
 app.use(morgan('dev'))
 
 app.use('/user', userRouter)
+
+app.get('/signup', (req, res) => {
+
+    res.render('signup')
+})
+
+app.get('/login', (req, res) => {
+
+    res.render('login')
+})
 
 const PORT = process.env.PORT || 7000
 
